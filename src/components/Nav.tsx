f1e5d645c41e968f466de62c48e06cc15767cf2c
sik2 <bickwang123@gmail.com>
2026-03-23 16:10:58 +0900

    0025

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,32 @@
 'use client'
 
 import { supabase } from '@/lib/supabase'
+import { User } from '@supabase/supabase-js'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 export default function Nav() {
-  const [user, setUser] = useState()
+  const [user, setUser] = useState<User | null>(null)
 
   const fetchUser = async () => {
     const {
       data: { user },
     } = await supabase.auth.getUser()
-    console.log('user', user)
     setUser(user)
   }
 
   useEffect(() => {
     fetchUser()
+
+    // 인증 상태(로그인/로그아웃) 변경을 감지하는 리스너
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      console.log('event', event)
+      setUser(session?.user ?? null)
+    })
+
+    // 클인업 함수 -> 컨포넌트가 언마운트 되거나 useEffect가 재실행되기 전
+    // 이벤트 리스터 중복 호출 방지
+    return () => data.subscription.unsubscribe()
   }, [])
 
   const handleOnLogout = async () => {
