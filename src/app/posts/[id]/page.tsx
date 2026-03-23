da99524fe551b7012a1bfdb3560f5ee24292e649
sik2 <bickwang123@gmail.com>
2026-03-23 11:11:44 +0900

    0016

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,23 +1,42 @@
 'use client'
 
+import { supabase } from '@/lib/supabase'
 import { useParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
+interface Post {
+  id: number
+  created_at: string
+  title: string
+  content: string
+}
+
 export default function PostDetail() {
   const { id } = useParams()
-  const [post, setPost] = useState({})
+  const [post, setPost] = useState<Post | null>(null)
+
+  const fetchPost = async () => {
+    const { data: post, error } = await supabase
+      .from('posts')
+      .select('*')
+      .eq('id', id as string)
+      .single()
+    setPost(post)
+  }
 
   useEffect(() => {
-    fetch(`https://dummyjson.com/posts/${id}`)
-      .then((res) => res.json())
-      .then((res) => setPost(res))
+    fetchPost()
   }, [])
+
+  if (!post) {
+    return <div>Loading...</div>
+  }
 
   return (
     <>
       <div>{post.id}번 게시글 상세</div>
       <div>{post.title}</div>
-      <div>{post.body}</div>
+      <div>{post.content}</div>
     </>
   )
 }
