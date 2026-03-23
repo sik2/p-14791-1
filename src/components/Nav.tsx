161255a7e5f1bb324529dc022e846c57405fd865
sik2 <bickwang123@gmail.com>
2026-03-23 15:18:45 +0900

    0024

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,33 @@
+'use client'
+
+import { supabase } from '@/lib/supabase'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export default function Nav() {
+  const [user, setUser] = useState()
+
+  const fetchUser = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
+    console.log('user', user)
+    setUser(user)
+  }
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
+  const handleOnLogout = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+      console.log(error)
+    } else {
+      alert('로그아웃 성공')
+    }
+  }
+
   return (
     <nav className="flex">
       <Link href="/" className="p-2 rounded hover:bg-gray-200">
@@ -9,12 +36,23 @@ export default function Nav() {
       <Link href="/posts" className="p-2 rounded hover:bg-gray-200">
         글목록
       </Link>
-      <Link href="/signup" className="p-2 rounded hover:bg-gray-200">
-        회원가입
-      </Link>
-      <Link href="/signin" className="p-2 rounded hover:bg-gray-200">
-        로그인
-      </Link>
+      {user ? (
+        <button
+          onClick={handleOnLogout}
+          className="p-2 rounded hover:bg-gray-200"
+        >
+          로그아웃
+        </button>
+      ) : (
+        <>
+          <Link href="/signup" className="p-2 rounded hover:bg-gray-200">
+            회원가입
+          </Link>
+          <Link href="/signin" className="p-2 rounded hover:bg-gray-200">
+            로그인
+          </Link>
+        </>
+      )}
     </nav>
   )
 }
